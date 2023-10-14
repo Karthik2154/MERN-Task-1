4d60ad8bb3db5e21505a2b0cc1a8794972412052
Karthik2154 <karthik.v2021@vitstudent.ac.in>
2023-10-14 22:46:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Header } from "./components/Header";
+import "./bootstrap-5.0.2-dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
